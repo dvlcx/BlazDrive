c665fc57eac71c252f3e51a5048a6fc4c912a569
dvlcx <ave.burning.goat.666@gmail.com>
2024-05-10 16:07:55 +0300

    main page

--- obj/Debug/net8.0/BlazDrive.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazDrive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41ebcbb952581903d2162af611761c38dc97894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e7f9b84589513cf4625a9c36eb60ed5a7f08b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
