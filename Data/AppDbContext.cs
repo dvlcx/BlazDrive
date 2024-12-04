97d24de3499e4478a39048758160145e46b10182
dvlcx <ave.burning.goat.666@gmail.com>
2024-12-04 18:36:49 +0300

    fix dbcontext warnings

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,10 +5,10 @@ namespace BlazDrive.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Folder> Folders { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Models.Entities.File> Files { get; set; }
-        public DbSet<DownloadLink> DownloadLinks { get; set; }
+        public DbSet<Folder> Folders { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Models.Entities.File> Files { get; set; } = null!;
+        public DbSet<DownloadLink> DownloadLinks { get; set; } = null!;
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
